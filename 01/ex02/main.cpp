503a01b96a5c913d511b7c49e71e39c4433c3b78
cerdelen <cedric.erdelen@web.de>
2022-07-22 13:15:10 +0200

    da

--- 01/ex02/main.cpp
+++ 01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cerdelen <cerdelen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 11:58:11 by cerdelen          #+#    #+#             */
-/*   Updated: 2022/07/11 11:58:12 by cerdelen         ###   ########.fr       */
+/*   Updated: 2022/07/21 11:27:06 by cerdelen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 
 int	main()
 {
-	std::string str = "HI THIS IS BRAIN";
-	std::string *ptr = &str;
-	std::string &ref = str;
+	std::string	str = "HI THIS IS BRAIN";
+	std::string	*ptr = &str;
+	std::string	&ref = str;
 
-	std::cout << "The memory address of the string variable = " << &str << std::endl 
-		<< "The memory address held by stringPTR = " << ptr << std::endl << 
-			"The memory address held by stringREF = " << &ref << std::endl << std:: endl;
+	std::cout << "The memory address of the string variable =             " << &str << std::endl 
+		<< "The memory address held by stringPTR =                  " << ptr << std::endl << 
+			"The memory address held by stringREF =                  " << &ref << std::endl << std:: endl;
 	
-	std::cout << "The value of the string variable = " << str << std::endl 
-		<< "The value pointed to by stringPTR = " << *ptr << std::endl << 
-			"The value pointed to by stringREF = " << ref << std::endl;
+	std::cout << "The value of the string variable =                      " << str << std::endl 
+		<< "The value pointed to by stringPTR =                     " << *ptr << std::endl << 
+			"The value pointed to by stringREF =                     " << ref << std::endl;
 	
 	
 }
