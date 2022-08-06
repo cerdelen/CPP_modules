5b1b4ff0db422f93d3ade23855624d8110aa4441
cerdelen <cedric.erdelen@web.de>
2022-08-06 16:40:05 +0200

    ready for eval

--- 01/ex03/Weapon.hpp
+++ 01/ex03/Weapon.hpp
@@ -6,7 +6,7 @@
 /*   By: cerdelen <cerdelen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 11:58:33 by cerdelen          #+#    #+#             */
-/*   Updated: 2022/07/11 11:58:35 by cerdelen         ###   ########.fr       */
+/*   Updated: 2022/08/06 16:12:02 by cerdelen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,15 @@
 
 class Weapon
 {
-private:
-	std::string type_;
-public:
-	Weapon(std::string type);
-	~Weapon();
-	const std::string& getType();
-	void setType(std::string t);
+	public:
+		Weapon(std::string type);
+		~Weapon();
+
+		const std::string&		getType();
+		void 					setType(std::string t);
+
+	private:
+		std::string		type_;
 };
 
 
