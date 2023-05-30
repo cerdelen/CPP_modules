0dcedefb1a1a7325cc86c3a2570a82572f91701f
Cedric Erdelen <cerdelen@c1s6d25.42wolfsburg.de>
2023-05-30 20:35:14 +0200

    bsp fixed

--- 02/ex03/bsp.cpp
+++ 02/ex03/bsp.cpp
@@ -6,55 +6,33 @@
 /*   By: cerdelen <cerdelen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 11:59:35 by cerdelen          #+#    #+#             */
-/*   Updated: 2022/07/11 11:59:36 by cerdelen         ###   ########.fr       */
+/*   Updated: 2023/05/30 20:26:50 by cerdelen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 #include "Fixed.hpp"
 
-typedef struct s_vec
+void	calculate_triangle_area(const Point &one, const Point &two, const Point& three, Fixed &res)
 {
-	Fixed	x;
-	Fixed	y;
-}	t_vec;
-
-Fixed	ft_dot(t_vec a, t_vec b)
-{
-	Fixed	out;
-
-	out = (a.x * b.x) + (a.y * b.y);
-	return (out);
+	res = (((one.getX() - three.getX()) * (two.getY() - three.getY())) - ((two.getX() - three.getX()) * (one.getY() - three.getY()))) / Fixed(2);
+	if (res < 0)
+		res = res * Fixed(-1);
 }
 
-bool bsp( const Point &a, const Point &b, const Point& c, const Point& point){
-t_vec	v0;
-t_vec	v1;
-t_vec	v2;
-// Compute vectors   
+bool bsp( const Point &a, const Point &b, const Point& c, const Point& point)
+{
+	Fixed	fullarea;
+	Fixed	firstarea;
+	Fixed	secondarea;
+	Fixed	thirdarea;
 
-v0.x = c.getX() - a.getX();
-v0.y = c.getY() - a.getY();
-
-v1.x = b.getX() - a.getX();
-v1.y = b.getY() - a.getY();
-
-v2.x = point.getX() - a.getX();
-v2.y = point.getY() - a.getY();
-
-
-// Compute dot products
-Fixed dot00 = ft_dot(v0, v0);
-Fixed dot01 = ft_dot(v0, v1);
-Fixed dot02 = ft_dot(v0, v2);
-Fixed dot11 = ft_dot(v1, v1);
-Fixed dot12 = ft_dot(v1, v2);
-
-// Compute barycentric coordinates
-Fixed	invDenom((Fixed) 1 / (dot00 * dot11 - dot01 * dot01));
-Fixed	u = (dot11 * dot02 - dot01 * dot12) * invDenom;
-Fixed	v = (dot00 * dot12 - dot01 * dot02) * invDenom;
-
-// Check if point is in triangle
-return (u >= 0) && (v >= 0) && (u + v < 1);
+	calculate_triangle_area(a, b, c, fullarea);
+	calculate_triangle_area(point, b, c, firstarea);
+	calculate_triangle_area(a, point, c, secondarea);
+	calculate_triangle_area(a, b, point, thirdarea);
+	
+	if ((firstarea + secondarea + thirdarea) > fullarea)
+		return (false);
+	return (true);
 }
--- 02/ex03/main.cpp
+++ 02/ex03/main.cpp
@@ -6,24 +6,33 @@
 /*   By: cerdelen <cerdelen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 11:59:42 by cerdelen          #+#    #+#             */
-/*   Updated: 2022/08/08 21:20:20 by cerdelen         ###   ########.fr       */
+/*   Updated: 2023/05/30 20:34:55 by cerdelen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include "Point.hpp"
 
-bool bsp( const Point &a, const Point &b, const Point& c, const Point& point);
+bool bsp( const Point &a, const Point &b, const Point& c, const Point& point );
+
+void	test_bsp(const Point &a, const Point &b, const Point& c, const Point& point, bool expected )
+{
+	static size_t	i = 0;
+	if (bsp(a, b, c, point) != expected)
+		std::cout << "test case nr: " << i << " failed" << std::endl;
+	else
+		std::cout << "test case nr: " << i << " passed" << std::endl;
+	i++;
+}
 
 int main(void)
 {
-	Point a(0, 0);
-	Point b(0.5f, 0.0f);
-	Point c(0, 10);
-	Point p(0, 0);
-
-	if (bsp(a, b, c, p) == true)
-		std::cout << "true" << std::endl;
-	else
-		std::cout << "false" << std::endl;
+	test_bsp(Point(0, 0), Point(10, 0), Point(0, 10), Point(5, 5), true);
+	test_bsp(Point(0, 0), Point(10, 0), Point(0, 10), Point(6, 5), false);
+	test_bsp(Point(0, 0), Point(10, 0), Point(0, 10), Point(5.1f, 5.0f), false);
+	test_bsp(Point(0, 0), Point(10, 0), Point(0, 10), Point(4.9f, 5.0f), true);
+	test_bsp(Point(0, 0), Point(10, 0), Point(0, 10), Point(4, 5), true);
+	test_bsp(Point(-1, 0), Point(10, 0), Point(0, 10), Point(4, 5), true);
+	test_bsp(Point(0, 0), Point(10, 0), Point(0, 10), Point(-4, 5), false);
+	test_bsp(Point(0.0f, 0.0f), Point(0.01f, 0.0f), Point(0.0f, 0.01f), Point(0.0001f, 0.0001f), true);
 }
